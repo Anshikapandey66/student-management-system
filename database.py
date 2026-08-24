d103ee9ff670e9c672671b95b4d022d418ee1f0a
Anshika Pandey <tanumansheer@gmail.com>
2026-08-24 22:18:46 +0530

    Make student marks fields non-nullable and format SQL
    Updated student marks fields to be non-nullable and modified SQL queries for better readability.

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 DATABASE_NAME = "students.db"
 
 
@@ -17,11 +18,11 @@ def create_table():
             name TEXT NOT NULL,
             age INTEGER NOT NULL,
             course TEXT NOT NULL,
-            python_marks REAL DEFAULT 0,
-            database_marks REAL DEFAULT 0,
-            math_marks REAL DEFAULT 0,
-            english_marks REAL DEFAULT 0,
-            computer_marks REAL DEFAULT 0
+            python_marks REAL NOT NULL,
+            database_marks REAL NOT NULL,
+            math_marks REAL NOT NULL,
+            english_marks REAL NOT NULL,
+            computer_marks REAL NOT NULL
         )
     """)
 
@@ -73,14 +74,18 @@ def get_all_students():
     connection = connect_database()
     cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM students")
+    cursor.execute("""
+        SELECT * FROM students
+        ORDER BY id
+    """)
 
     students = cursor.fetchall()
 
     connection.close()
 
     return students
-    
+
+
 def search_student(keyword):
     connection = connect_database()
     cursor = connection.cursor()
@@ -95,6 +100,72 @@ def search_student(keyword):
     connection.close()
 
     return students
+
+
+def update_student(
+    student_id,
+    name,
+    age,
+    course,
+    python_marks,
+    database_marks,
+    math_marks,
+    english_marks,
+    computer_marks
+):
+    connection = connect_database()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        UPDATE students
+        SET
+            name = ?,
+            age = ?,
+            course = ?,
+            python_marks = ?,
+            database_marks = ?,
+            math_marks = ?,
+            english_marks = ?,
+            computer_marks = ?
+        WHERE id = ?
+    """, (
+        name,
+        age,
+        course,
+        python_marks,
+        database_marks,
+        math_marks,
+        english_marks,
+        computer_marks,
+        student_id
+    ))
+
+    connection.commit()
+
+    updated = cursor.rowcount
+
+    connection.close()
+
+    return updated
+
+
+def delete_student(student_id):
+    connection = connect_database()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        DELETE FROM students
+        WHERE id = ?
+    """, (student_id,))
+
+    connection.commit()
+
+    deleted = cursor.rowcount
+
+    connection.close()
+
+    return deleted
+
 
 if __name__ == "__main__":
     create_table()
